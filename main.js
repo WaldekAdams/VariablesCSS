64dedbd0d3b225fd11dba4b514baa8af00e32656
Waldemar Adams <waldek-adamski@wp.pl>
2020-05-13 12:39:03 +0200

    add function 'changeValue', modify index.html

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const inputs = document.querySelectorAll('.controls input')
 
 changeValue = function () {
     console.log(this.value)
+    const suffix = this.dataset.sizing || ''
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    // document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', changeValue))
